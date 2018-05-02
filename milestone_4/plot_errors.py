87c22ed537d63f21234d060f3c66b5eb3a50e185
zachglick <glickzachary@gmail.com>
2018-05-02 16:45:03 -0500

    pcr graphs

--- milestone_4/plot_errors.py
+++ milestone_4/plot_errors.py
@@ -20,11 +20,34 @@ with open('error_data/test_error_Ridge.csv') as f:
     lines = f.readlines()
     Ridge_test = np.array(lines, dtype = np.float32)
 
+
+with open('error_data/train_error_pca2.csv') as f:
+    lines = f.readlines()
+    pca2_train = np.array(lines, dtype = np.float32)
+
+
+with open('error_data/test_error_pca2.csv') as f:
+    lines = f.readlines()
+    pca2_test = np.array(lines, dtype = np.float32)
+
+
+with open('error_data/train_error_pca3.csv') as f:
+    lines = f.readlines()
+    pca3_train = np.array(lines, dtype = np.float32)
+
+
+with open('error_data/test_error_pca3.csv') as f:
+    lines = f.readlines()
+    pca3_test = np.array(lines, dtype = np.float32)
+
 GP_train_av = np.average(GP_train)
 GP_test_av = np.average(GP_test)
 Ridge_train_av = np.average(Ridge_train)
 Ridge_test_av = np.average(Ridge_test)
-
+pca2_train_av = np.average(pca2_train)
+pca2_test_av = np.average(pca2_test)
+pca3_train_av = np.average(pca3_train)
+pca3_test_av = np.average(pca3_test)
 
 plt.scatter(GP_train, GP_test, label="samples")
 plt.scatter([GP_train_av], [GP_test_av], label="average")
@@ -47,21 +70,39 @@ plt.show()
 
 
 
+plt.scatter(pca2_train, pca2_test, label = "samples")
+plt.scatter([pca2_train_av], [pca2_test_av], label="average")
+plt.xlabel("Training MAE")
+plt.ylabel("Testing MAE")
+plt.title('Principal 2-Component Regression Errors')
+plt.legend()
+plt.savefig('graphs/pca2.png')
+plt.show()
+
+
+
+plt.scatter(pca3_train, pca3_test, label = "samples")
+plt.scatter([pca3_train_av], [pca3_test_av], label="average")
+plt.xlabel("Training MAE")
+plt.ylabel("Testing MAE")
+plt.title('Principal 3-Component Regression Errors')
+plt.legend()
+plt.savefig('graphs/pca3.png')
+plt.show()
 
 
 
 
-
-
-
-plt.scatter(GP_train, GP_test, label="GP samples")
 plt.scatter(Ridge_train, Ridge_test, label = "Ridge samples")
+plt.scatter(GP_train, GP_test, label="GP samples")
+plt.scatter(pca2_train, pca2_test, label = "2PCR samples")
+plt.scatter(pca3_train, pca3_test, label = "3PCR samples")
 #plt.scatter([GP_train_av], [GP_test_av], label="GP average")
 #plt.scatter([Ridge_train_av], [Ridge_test_av], label="Ridge average")
 plt.xlabel("Training MAE")
 plt.ylabel("Testing MAE")
 plt.title('Comparative Errors')
-plt.legend()
+plt.legend(loc='upper right')
 plt.savefig('graphs/Comparison.png')
 plt.show()
 
--- optional_9/plot_times.py
+++ optional_9/plot_times.py
@@ -20,10 +20,26 @@ with open('timing_data/test_time_Ridge.csv') as f:
     lines = f.readlines()
     Ridge_test = np.array(lines, dtype = np.float32)
 
-print(len(GP_train))
-print(len(GP_test))
-print(len(Ridge_train))
-print(len(Ridge_test))
+
+
+with open('timing_data/train_time_pca2.csv') as f:
+    lines = f.readlines()
+    pca2_train = np.array(lines, dtype = np.float32)
+
+
+with open('timing_data/test_time_pca2.csv') as f:
+    lines = f.readlines()
+    pca2_test = np.array(lines, dtype = np.float32)
+
+
+with open('timing_data/train_time_pca3.csv') as f:
+    lines = f.readlines()
+    pca3_train = np.array(lines, dtype = np.float32)
+
+
+with open('timing_data/test_time_pca3.csv') as f:
+    lines = f.readlines()
+    pca3_test = np.array(lines, dtype = np.float32)
 
 
 plt.hist(GP_train)
@@ -59,6 +75,34 @@ plt.show()
 
 
 
+plt.hist(pca2_train)
+plt.xlabel('Training Time (sec)')
+plt.ylabel('Count')
+plt.title('Histogram of Principal 2-Component Regression Training')
+plt.savefig('graphs/pca2_train.png')
+plt.show()
+
+
+plt.hist(pca2_test)
+plt.xlabel('Testing Time (sec)')
+plt.ylabel('Count')
+plt.title('Histogram of Principal 2-Component Regression Testing')
+plt.savefig('graphs/pca2_test.png')
+plt.show()
 
 
 
+plt.hist(pca3_train)
+plt.xlabel('Training Time (sec)')
+plt.ylabel('Count')
+plt.title('Histogram of Principal 3-Component Regression Training')
+plt.savefig('graphs/pca3_train.png')
+plt.show()
+
+
+plt.hist(pca3_test)
+plt.xlabel('Testing Time (sec)')
+plt.ylabel('Count')
+plt.title('Histogram of Principal 3-Component Regression Testing')
+plt.savefig('graphs/pca3_test.png')
+plt.show()
