4e4cea79e50e9ccf8f53af9fbbe1237c8b14645c
zachglick <glickzachary@gmail.com>
2018-05-02 18:04:50 -0500

    SVM stuff

--- milestone_4/plot_errors.py
+++ milestone_4/plot_errors.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
 
 with open('error_data/train_error_GP.csv') as f:
     lines = f.readlines()
@@ -20,6 +21,15 @@ with open('error_data/test_error_Ridge.csv') as f:
     lines = f.readlines()
     Ridge_test = np.array(lines, dtype = np.float32)
 
+
+with open('error_data/train_error_SVM.csv') as f:
+    lines = f.readlines()
+    SVM_train = np.array(lines, dtype = np.float32)
+
+
+with open('error_data/test_error_SVM.csv') as f:
+    lines = f.readlines()
+    SVM_test = np.array(lines, dtype = np.float32)
 
 with open('error_data/train_error_pca2.csv') as f:
     lines = f.readlines()
@@ -44,10 +54,49 @@ GP_train_av = np.average(GP_train)
 GP_test_av = np.average(GP_test)
 Ridge_train_av = np.average(Ridge_train)
 Ridge_test_av = np.average(Ridge_test)
+SVM_train_av = np.average(SVM_train)
+SVM_test_av = np.average(SVM_test)
 pca2_train_av = np.average(pca2_train)
 pca2_test_av = np.average(pca2_test)
 pca3_train_av = np.average(pca3_train)
 pca3_test_av = np.average(pca3_test)
+
+
+
+
+tstat, pval = ttest_ind(Ridge_test,GP_test) 
+print("T-statistic, p-value for Ridge and GP: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(Ridge_test,SVM_test) 
+print("T-statistic, p-value for Ridge and SVM: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(Ridge_test,pca2_test) 
+print("T-statistic, p-value for Ridge and 2PCR: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(Ridge_test,pca3_test) 
+print("T-statistic, p-value for Ridge and 3PCR: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(GP_test,SVM_test) 
+print("T-statistic, p-value for GP and SVM: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(GP_test,pca2_test) 
+print("T-statistic, p-value for GP and pca2: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(GP_test,pca3_test) 
+print("T-statistic, p-value for GP and pca3: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(SVM_test,pca2_test) 
+print("T-statistic, p-value for SVM and pca2: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(SVM_test,pca3_test) 
+print("T-statistic, p-value for SVM and pca3: (%f %g) " % (tstat,pval)) 
+ 
+tstat, pval = ttest_ind(pca2_test,pca3_test) 
+print("T-statistic, p-value for pca2 and pca3: (%f %g) " % (tstat,pval)) 
+
+
+
+
 
 plt.scatter(GP_train, GP_test, label="samples")
 plt.scatter([GP_train_av], [GP_test_av], label="average")
@@ -66,6 +115,16 @@ plt.ylabel("Testing MAE")
 plt.title('Ridge Regression Errors')
 plt.legend()
 plt.savefig('graphs/Ridge.png')
+plt.show()
+
+
+plt.scatter(SVM_train, SVM_test, label = "samples")
+plt.scatter([SVM_train_av], [SVM_test_av], label="average")
+plt.xlabel("Training MAE")
+plt.ylabel("Testing MAE")
+plt.title('SVM Regression Errors')
+plt.legend()
+plt.savefig('graphs/SVM.png')
 plt.show()
 
 
@@ -95,10 +154,9 @@ plt.show()
 
 plt.scatter(Ridge_train, Ridge_test, label = "Ridge samples")
 plt.scatter(GP_train, GP_test, label="GP samples")
+plt.scatter(SVM_train, SVM_test, label="SVM samples")
 plt.scatter(pca2_train, pca2_test, label = "2PCR samples")
 plt.scatter(pca3_train, pca3_test, label = "3PCR samples")
-#plt.scatter([GP_train_av], [GP_test_av], label="GP average")
-#plt.scatter([Ridge_train_av], [Ridge_test_av], label="Ridge average")
 plt.xlabel("Training MAE")
 plt.ylabel("Testing MAE")
 plt.title('Comparative Errors')
--- optional_9/plot_times.py
+++ optional_9/plot_times.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import ttest_ind
 
 with open('timing_data/train_time_GP.csv') as f:
     lines = f.readlines()
@@ -21,7 +22,6 @@ with open('timing_data/test_time_Ridge.csv') as f:
     Ridge_test = np.array(lines, dtype = np.float32)
 
 
-
 with open('timing_data/train_time_pca2.csv') as f:
     lines = f.readlines()
     pca2_train = np.array(lines, dtype = np.float32)
@@ -41,6 +41,44 @@ with open('timing_data/test_time_pca3.csv') as f:
     lines = f.readlines()
     pca3_test = np.array(lines, dtype = np.float32)
 
+Ridge = Ridge_train+Ridge_test
+GP = GP_train+GP_test
+SVM = Ridge #SVM_train+SVM_test
+pca2 = pca2_train+pca2_test
+pca3 = pca3_train+pca3_test
+
+
+
+
+tstat, pval = ttest_ind(Ridge,GP)
+print("T-statistic, p-value for Ridge and GP: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(Ridge,SVM)
+print("T-statistic, p-value for Ridge and SVM: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(Ridge,pca2)
+print("T-statistic, p-value for Ridge and 2PCR: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(Ridge,pca3)
+print("T-statistic, p-value for Ridge and 3PCR: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(GP,SVM)
+print("T-statistic, p-value for GP and SVM: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(GP,pca2)
+print("T-statistic, p-value for GP and pca2: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(GP,pca3)
+print("T-statistic, p-value for GP and pca3: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(SVM,pca2)
+print("T-statistic, p-value for SVM and pca2: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(SVM,pca3)
+print("T-statistic, p-value for SVM and pca3: (%f %g) " % (tstat,pval))
+
+tstat, pval = ttest_ind(pca2,pca3)
+print("T-statistic, p-value for pca2 and pca3: (%f %g) " % (tstat,pval))
 
 plt.hist(GP_train)
 plt.xlabel('Training Time (sec)')
@@ -74,7 +112,6 @@ plt.savefig('graphs/Ridge_test.png')
 plt.show()
 
 
-
 plt.hist(pca2_train)
 plt.xlabel('Training Time (sec)')
 plt.ylabel('Count')
@@ -89,7 +126,6 @@ plt.ylabel('Count')
 plt.title('Histogram of Principal 2-Component Regression Testing')
 plt.savefig('graphs/pca2_test.png')
 plt.show()
-
 
 
 plt.hist(pca3_train)
